a347d646d13e0cf6e616e6c3eb87191bc22332f1
supriyamishra <supriyamishra739@gmail.com>
2025-08-04 11:22:38 +0530

    new files

--- 2pointerAritmetic.c
+++ 2pointerAritmetic.c
@@ -9,4 +9,7 @@ int main()
     printf("%d\n", p);
     printf("%d\n", sizeof(int));
     printf("%d\n", p + 1);
+
+    printf("value at location p is :%d\n", *p);
+    printf("value at location p+1 is :%d\n", *(p + 1));
 }
