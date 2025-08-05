477f5a074be6e112898ce011ff72a20bba20a95c
supriyamishra <supriyamishra739@gmail.com>
2025-08-05 13:37:31 +0530

    done more ques on pointers

--- PointerPractice/5maximum-element-in-array-using-pointers.c
+++ PointerPractice/5maximum-element-in-array-using-pointers.c
@@ -2,6 +2,25 @@
 
 // Q) Find the maximum element in an array using pointers.
 
+int maxElementInArray(int *p, int size)
+{
+    int max = *p;
+
+    for (int i = 1; i < size; i++)
+    {
+        p++; // Move to the next element
+        if (*p > max)
+        {
+            max = *p;
+        }
+        }
+    return max;
+}
+
 int main()
 {
+    int A[] = {1, 2, 3, 5, 0, 2};
+    int size = sizeof(A) / sizeof(A[0]);
+    int ans = maxElementInArray(A, size);
+    printf("the max element is %d\n", ans);
 }
